2508a97555ee090e53513fe799e0d78e9f535608
jason121301 <jason121301@gmail.com>
2023-03-12 10:39:54 -0400

    finished properties

--- Restify/Restify/settings.py
+++ Restify/Restify/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'accounts',
+    'properties',
 ]
 
 REST_FRAMEWORK = {
---
+++ Restify/properties/serializers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
+from django.core.validators import validate_email
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, ValidationError
+
+from .models import Property, Amenity, PropertyImage, Reservation
+
+
+
+
+class propertyCreateSerializer(ModelSerializer):
+    class Meta:
+        model = Property
+        fields = ['address', 'description', 'guests', 'beds', 'bathrooms', 'amenities']
+    
+    def validate(self, data):
+        amenities = data.get('amenities')
+        if amenities:
+            for amenity in amenities:
+                if not Amenity.objects.filter(id=amenity.id).exists():
+                    raise serializers.ValidationError('Amenity with id={} does not exist.'.format(amenity.id))
+        return data
+    
+class propertyImageCreator(ModelSerializer):
+    class Meta:
+        model = PropertyImage
+        fields =['name','image','default']
+    
+class reservationCreator(ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = ['guest', 'start_date', 'end_date', 'state']
