f8aadb2bc2b83f5914bc7a98a06f1158546a4311
ashenafee <ashenafeem@gmail.com>
2023-03-11 20:08:41 -0500

    Revoked old SECRET_KEY and loaded new one from .env

--- Restify/Restify/settings.py
+++ Restify/Restify/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7r+(73wdv^tia6b+eru&@vcoot9y+ocf&z50(ki)&r^vg0m!lg'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
