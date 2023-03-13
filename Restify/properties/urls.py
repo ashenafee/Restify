9b0a6d4f33fcfd8c004679c2e95613f8e21f0cfe
jason121301 <jason121301@gmail.com>
2023-03-12 23:54:11 -0400

    stupid dumb migrations, but now editing reservations, properties is done

--- Restify/properties/urls.py
+++ Restify/properties/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from .views import propertyCreateView,propertyImageCreateView,ReservationCreateView
+from .views import propertyCreateView,propertyImageCreateView,ReservationCreateView, PropertyUpdateView,ReservationUpdateStateView
 
 app_name = 'properties'
 
 urlpatterns = [
-    path('add/', propertyCreateView.as_view(), name='property_create'),
-    path('add/<int:property_id>/images/',propertyImageCreateView.as_view(), name='image_add'),
-    path('add/<int:property_id>/reservation/', ReservationCreateView.as_view(), name='reservation_add')
+    path('property/add/', propertyCreateView.as_view(), name='property_create'),
+    path('images/<int:property_id>/add/',propertyImageCreateView.as_view(), name='image_add'),
+    path('reservation/<int:property_id>/add/', ReservationCreateView.as_view(), name='reservation_add'),
+    path('property/<int:property_id>/edit/', PropertyUpdateView.as_view(), name='property_update'),
+    path('reservation/<int:reservation_id>/update/', ReservationUpdateStateView.as_view(), name='reservation_update'),
 ]
-
-
