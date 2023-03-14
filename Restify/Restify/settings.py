a5fb1c6ae0cae14a818cf82283357613f0f47d13
stankate <stankattee@gmail.com>
2023-03-13 23:47:38 -0400

    new page_size

--- Restify/Restify/settings.py
+++ Restify/Restify/settings.py
@@ -56,7 +56,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 9,
+    'PAGE_SIZE': 4,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 
 }
