d375a946caae2fbe768c5e85d98c8c1be97f6f3d
Ashenafee Mandefro <ashenafeem@gmail.com>
2023-03-14 20:56:20 -0400

    Created base notification model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
