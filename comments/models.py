033b2837374355498ef6c797c654f7714d22fc2e
ashenafee <ashenafeem@gmail.com>
2023-03-15 13:16:42 -0400

    Added changes from remote

--- comments/models.py
+++ comments/models.py
@@ -17,7 +17,7 @@ class Comment(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     text = models.TextField()
 
-
+#comment left by a host
 class HostComment(Comment):
     # Guest who the comment is about
     guest = models.ForeignKey(User, on_delete=models.CASCADE, related_name='guest_being_commented')
@@ -26,11 +26,11 @@ class HostComment(Comment):
     def __str__(self):
         return f'Host {self.author.first_name} commented on {self.guest.first_name}\'s stay at {self.reservation.property.name}'
 
-
+#comment left by a guest
 class GuestComment(Comment):
     # Host who the comment is about
     host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='host_being_commented')
-    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='commentsOftheProperty')
 
     def __str__(self):
         return f'Guest {self.author.first_name} commented on {self.host.first_name}\'s property {self.property.name}'
@@ -42,7 +42,7 @@ class Reply(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     text = models.TextField()
 
-
+#host reply to a guest comment
 class HostReply(Reply):
     # Comment that the reply is about
     comment = models.ForeignKey(GuestComment, on_delete=models.CASCADE)
@@ -50,7 +50,7 @@ class HostReply(Reply):
     def __str__(self):
         return f'Host {self.author.first_name} replied to {self.comment.author.first_name}\'s comment on {self.comment.reservation.property.name}'
 
-
+#guest reply to a host comment
 class GuestReply(Reply):
     # Comment that the reply is about
     comment = models.ForeignKey(HostComment, on_delete=models.CASCADE)
