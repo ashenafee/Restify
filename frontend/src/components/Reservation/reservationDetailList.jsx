45ce027d182bd8533efc4df96b0312be21882a75
ashenafee <ashenafeem@gmail.com>
2023-04-16 18:10:52 -0400

    Fixed issue with "history"

--- frontend/src/components/Reservation/reservationDetailList.jsx
+++ frontend/src/components/Reservation/reservationDetailList.jsx
@@ -50,11 +50,6 @@ function ReservationDetailList() {
   const handleFilterSubmit = async (e) => {
     e.preventDefault();
     const access_token = localStorage.getItem("access_token");
-    if (!access_token) {
-      history.push("/login");
-      return;
-    }
-  
     const { role, state } = filterState;
     let url = "http://localhost:8000/properties/reservation/list/?page=1";
     setCurrentPage(1);
