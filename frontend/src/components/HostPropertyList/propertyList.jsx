47518dead1a700c1d73cbc204e2dc1e9bad1d9d0
stankate <stankattee@gmail.com>
2023-04-17 12:04:28 -0400

    redirect to login

--- frontend/src/components/HostPropertyList/propertyList.jsx
+++ frontend/src/components/HostPropertyList/propertyList.jsx
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from "react";
 import PropertyCard from "./propertyCard";
 import { Container, Row, Col } from "react-bootstrap";
 import { H1 } from "../Common/Headers";
+import { useNavigate } from "react-router-dom";
+import { ButtonFilled } from '../Common/Button';
 
 const MyProperties = () => {
     const [properties, setProperties] = useState([]);
+    const [unauthorized, setUnauthorized] = useState(false); // State for unauthorized status
+    const navigate = useNavigate();
+
 
     useEffect(() => {
         // Fetch properties from the API
@@ -13,30 +18,57 @@ const MyProperties = () => {
                 Authorization: `Bearer ${localStorage.getItem('access_token')}`,
             },
         })
-            .then((response) => response.json())
+            // redirect to login
+            .then((response) => {
+                // Check if response is unauthorized
+                if (response.status === 401) {
+                    setUnauthorized(true);
+                    setTimeout(() => {
+                        navigate("/login");
+                    }, 2000); // 2 seconds delay
+                    return;
+                }
+                return response.json();
+            })
             .then((data) => setProperties(data))
             .catch((error) => console.error('Error fetching properties:', error));
     }, []);
 
     return (
         <div>
-            <H1 value={"My Properties"} />
+            <H1 value={"My Properties"} className = "mt-3"/>
 
             <div>
-                {Array.isArray(properties) && properties.length > 0 ? (
-                    // if properties exist
-                    <Container>
-                        <Row>
-                            {properties.map((property) => (
-                                <Col key={property.id} xs={12} sm={6} md={4} lg={4} xl={4}>
-                                    <PropertyCard property={property} />
-                                </Col>
-                            ))}
-                        </Row>
-                    </Container>
+                {unauthorized ? ( // Use unauthorized status to conditionally render message
+                    <p className="text-center">You are not authorized. Redirecting to login page...</p>
                 ) : (
-                    // if no properties exist
-                    <p className="text-center">No properties found</p>
+                    Array.isArray(properties) && properties.length > 0 ? (
+                        // if properties exist
+                        <Container>
+                            <Row>
+                                {properties.map((property) => (
+                                    <Col key={property.id} xs={12} sm={6} md={4} lg={4} xl={4}>
+                                        <PropertyCard property={property} />
+                                    </Col>
+                                ))}
+                            </Row>
+
+                            <Row>
+                                <ButtonFilled 
+                                    value={"Add Property"} 
+                                    onClick={() => navigate("/property/create")} />
+                            </Row>
+                        </Container>
+                    ) : (
+                        // if no properties exist
+                        <div>
+                            <p className="text-center">No properties found</p>
+
+                            <ButtonFilled 
+                                value={"Add Property"} 
+                                onClick={() => navigate("/property/create")} />
+                        </div>
+                    )
                 )}
             </div>
 
